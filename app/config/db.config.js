0fe5d558d76f7c0bc0b4048a27e2af426faff116
Md Risfat <Risfat.bd@gmail.com>
2023-04-02 00:36:45 +0600

    Initial commit...

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "test-rds-free.comki5n2mwwd.ap-southeast-1.rds.amazonaws.com",
+  USER: "root",
+  PASSWORD: "toor12345678",
+  DB: "testdb"
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+// const bodyParser = require("body-parser"); /* deprecated */
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+// const db = require("./app/models/tutorial.model");
+// db.sequelize.sync();
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json()); /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "What are you doing here ! You shouldn't be sneak in here." });
+});
+
+require("./app/routes/tutorial.routes.js")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
