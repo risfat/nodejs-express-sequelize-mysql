3d311d39161278e9bc8fbc4361410d39ff367bda
Md Risfat <Risfat.bd@gmail.com>
2023-04-02 01:18:23 +0600

    Minor Bugs Fixed...

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8080"
 };
 
 // const db = require("./app/models/tutorial.model");
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 require("./app/routes/tutorial.routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
